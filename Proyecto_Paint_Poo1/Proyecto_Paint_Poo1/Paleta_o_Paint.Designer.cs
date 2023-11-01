942992f055c02ef3a8269a0a212a01cb9857063f
Ulisesdelvillar0207 <ur226278@gmail.com>
2023-11-01 15:25:21 -0600

    agregado 1

---
+++ Proyecto_Paint_Poo1/Proyecto_Paint_Poo1/Paleta_o_Paint.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace Proyecto_Paint_Poo1
+{
+    partial class Paleta_o_Paint
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.tablaDeColores = new System.Windows.Forms.TableLayoutPanel();
+            this.btnColorSeleccionado = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnBorrador = new System.Windows.Forms.Button();
+            this.btnLapiz = new System.Windows.Forms.Button();
+            this.btnRellenar = new System.Windows.Forms.Button();
+            this.btnLinea = new System.Windows.Forms.Button();
+            this.btnCirculo = new System.Windows.Forms.Button();
+            this.btnCuadrado = new System.Windows.Forms.Button();
+            this.pbLienzo = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbLienzo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.tablaDeColores);
+            this.splitContainer1.Panel1.Controls.Add(this.btnColorSeleccionado);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.panel1);
+            this.splitContainer1.Panel2.Controls.Add(this.pbLienzo);
+            this.splitContainer1.Size = new System.Drawing.Size(916, 721);
+            this.splitContainer1.SplitterDistance = 262;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // tablaDeColores
+            // 
+            this.tablaDeColores.ColumnCount = 2;
+            this.tablaDeColores.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tablaDeColores.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tablaDeColores.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.tablaDeColores.Location = new System.Drawing.Point(3, 3);
+            this.tablaDeColores.Name = "tablaDeColores";
+            this.tablaDeColores.RowCount = 8;
+            this.tablaDeColores.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 48.17518F));
+            this.tablaDeColores.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 51.82482F));
+            this.tablaDeColores.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 74F));
+            this.tablaDeColores.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 82F));
+            this.tablaDeColores.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 75F));
+            this.tablaDeColores.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 82F));
+            this.tablaDeColores.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 79F));
+            this.tablaDeColores.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 77F));
+            this.tablaDeColores.Size = new System.Drawing.Size(256, 607);
+            this.tablaDeColores.TabIndex = 0;
+            // 
+            // btnColorSeleccionado
+            // 
+            this.btnColorSeleccionado.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnColorSeleccionado.Location = new System.Drawing.Point(97, 632);
+            this.btnColorSeleccionado.Name = "btnColorSeleccionado";
+            this.btnColorSeleccionado.Size = new System.Drawing.Size(50, 50);
+            this.btnColorSeleccionado.TabIndex = 1;
+            this.btnColorSeleccionado.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnBorrador);
+            this.panel1.Controls.Add(this.btnLapiz);
+            this.panel1.Controls.Add(this.btnRellenar);
+            this.panel1.Controls.Add(this.btnLinea);
+            this.panel1.Controls.Add(this.btnCirculo);
+            this.panel1.Controls.Add(this.btnCuadrado);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 621);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(650, 100);
+            this.panel1.TabIndex = 1;
+            // 
+            // btnBorrador
+            // 
+            this.btnBorrador.BackgroundImage = global::Proyecto_Paint_Poo1.Properties.Resources.borrador;
+            this.btnBorrador.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnBorrador.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnBorrador.Location = new System.Drawing.Point(313, 11);
+            this.btnBorrador.Name = "btnBorrador";
+            this.btnBorrador.Size = new System.Drawing.Size(50, 50);
+            this.btnBorrador.TabIndex = 6;
+            this.btnBorrador.UseVisualStyleBackColor = true;
+            this.btnBorrador.Click += new System.EventHandler(this.btnBorrador_Click);
+            // 
+            // btnLapiz
+            // 
+            this.btnLapiz.BackgroundImage = global::Proyecto_Paint_Poo1.Properties.Resources.lapiz;
+            this.btnLapiz.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnLapiz.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLapiz.Location = new System.Drawing.Point(201, 11);
+            this.btnLapiz.Name = "btnLapiz";
+            this.btnLapiz.Size = new System.Drawing.Size(50, 50);
+            this.btnLapiz.TabIndex = 5;
+            this.btnLapiz.UseVisualStyleBackColor = true;
+            this.btnLapiz.Click += new System.EventHandler(this.btnLapiz_Click);
+            // 
+            // btnRellenar
+            // 
+            this.btnRellenar.BackgroundImage = global::Proyecto_Paint_Poo1.Properties.Resources.pintura_en_aerosol;
+            this.btnRellenar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnRellenar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnRellenar.Location = new System.Drawing.Point(257, 11);
+            this.btnRellenar.Name = "btnRellenar";
+            this.btnRellenar.Size = new System.Drawing.Size(50, 50);
+            this.btnRellenar.TabIndex = 4;
+            this.btnRellenar.UseVisualStyleBackColor = true;
+            // 
+            // btnLinea
+            // 
+            this.btnLinea.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnLinea.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLinea.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLinea.Location = new System.Drawing.Point(145, 11);
+            this.btnLinea.Name = "btnLinea";
+            this.btnLinea.Size = new System.Drawing.Size(50, 50);
+            this.btnLinea.TabIndex = 3;
+            this.btnLinea.Text = "/";
+            this.btnLinea.UseVisualStyleBackColor = true;
+            this.btnLinea.Click += new System.EventHandler(this.btnLinea_Click);
+            // 
+            // btnCirculo
+            // 
+            this.btnCirculo.BackgroundImage = global::Proyecto_Paint_Poo1.Properties.Resources.Circulo;
+            this.btnCirculo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnCirculo.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCirculo.Location = new System.Drawing.Point(89, 11);
+            this.btnCirculo.Name = "btnCirculo";
+            this.btnCirculo.Size = new System.Drawing.Size(50, 50);
+            this.btnCirculo.TabIndex = 2;
+            this.btnCirculo.UseVisualStyleBackColor = true;
+            this.btnCirculo.Click += new System.EventHandler(this.btnCirculo_Click);
+            // 
+            // btnCuadrado
+            // 
+            this.btnCuadrado.BackgroundImage = global::Proyecto_Paint_Poo1.Properties.Resources.Cuadrado;
+            this.btnCuadrado.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnCuadrado.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCuadrado.Location = new System.Drawing.Point(33, 11);
+            this.btnCuadrado.Name = "btnCuadrado";
+            this.btnCuadrado.Size = new System.Drawing.Size(50, 50);
+            this.btnCuadrado.TabIndex = 0;
+            this.btnCuadrado.UseVisualStyleBackColor = true;
+            this.btnCuadrado.Click += new System.EventHandler(this.btnCuadrado_Click);
+            // 
+            // pbLienzo
+            // 
+            this.pbLienzo.Location = new System.Drawing.Point(-73, 3);
+            this.pbLienzo.Name = "pbLienzo";
+            this.pbLienzo.Size = new System.Drawing.Size(592, 607);
+            this.pbLienzo.TabIndex = 0;
+            this.pbLienzo.TabStop = false;
+            this.pbLienzo.Paint += new System.Windows.Forms.PaintEventHandler(this.pbLienzo_Paint);
+            this.pbLienzo.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pbLienzo_MouseDown);
+            this.pbLienzo.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pbLienzo_MouseMove);
+            this.pbLienzo.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pbLienzo_MouseUp);
+            // 
+            // Paleta_o_Paint
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(916, 721);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Paleta_o_Paint";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Paleta_o_Paint";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbLienzo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.PictureBox pbLienzo;
+        private System.Windows.Forms.TableLayoutPanel tablaDeColores;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnCuadrado;
+        private System.Windows.Forms.Button btnLinea;
+        private System.Windows.Forms.Button btnCirculo;
+        private System.Windows.Forms.Button btnColorSeleccionado;
+        private System.Windows.Forms.Button btnBorrador;
+        private System.Windows.Forms.Button btnLapiz;
+        private System.Windows.Forms.Button btnRellenar;
+    }
+}
---
+++ Proyecto_Paint_Poo1/Proyecto_Paint_Poo1/Paleta_o_Paint.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace Proyecto_Paint_Poo1
+{
+    public partial class Paleta_o_Paint : Form
+    {
+        private Color[] paletteColors;
+        Bitmap Mb;
+        Graphics g;
+        bool pintar = false;
+        Point px, py;
+        Pen pluma = new Pen(Color.Black, 1);
+        Pen borrador = new Pen(Color.White, 50);
+        int indice;
+        int x, y, sX, sY, cX, cY;
+
+        public Paleta_o_Paint()
+        {
+            InitializeComponent(); // Inicializa la interfaz de usuario del formulario.
+
+            Mb = new Bitmap(pbLienzo.Width, pbLienzo.Height);
+            g = Graphics.FromImage(Mb);
+            pbLienzo.Image = Mb;
+
+            paletteColors = new[] {
+                Color.Black, Color.White, Color.Gray, Color.Maroon, Color.Orange,
+                Color.Red, Color.Purple, Color.Fuchsia, Color.Green, Color.Lime,
+                Color.Olive, Color.Yellow, Color.Navy, Color.Blue, Color.Teal, Color.Aqua
+            };
+
+            // Crea una paleta de colores con 16 colores predefinidos .
+            for (int i = 0; i < 16; i++)
+            {
+                var colorBtn = new Button(); // Crea un nuevo botón.
+                tablaDeColores.Controls.Add(colorBtn, i % 2, i / 2); // Agrega el botón a un panel.
+                colorBtn.Dock = DockStyle.Fill;
+                colorBtn.FlatStyle = FlatStyle.Flat;
+                colorBtn.BackColor = paletteColors[i]; // Establece el color de fondo del botón.
+                colorBtn.MouseDown += ColorBtn_MouseDown; // Asigna el mismo evento a todos los botones.
+            }
+        }
+
+        private void ColorBtn_MouseDown(object sender, MouseEventArgs e)
+        {
+            switch (e.Button)
+            {
+                case MouseButtons.Left:
+                    ColorBtnGetColor(sender); // Llama a la función para obtener el color del botón.
+                    break;
+                case MouseButtons.Right:
+                    ColorBtnColorPicker(sender); // Llama a la función para seleccionar un color personalizado.
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void ColorBtnColorPicker(object sender)
+        {
+            var btn = (Button)sender;
+            var clrDialog = new ColorDialog() { FullOpen = true, AnyColor = true };
+            if (clrDialog.ShowDialog() == DialogResult.OK)
+                btn.BackColor = clrDialog.Color; // Actualiza el color de fondo del botón con el color seleccionado.
+        }
+
+        private void ColorBtnGetColor(object sender)
+        {
+            var btn = (Button)sender;
+            pbLienzo.BackColor = btn.BackColor; // Actualiza el color de fondo de un objeto llamado "pbLienzo" (Es un PictureBox).
+            btnColorSeleccionado.BackColor = btn.BackColor;
+            pluma.Color = btn.BackColor;
+        }
+
+
+        private void pbLienzo_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            if (pintar)
+            {
+                if (indice == 3)
+                {
+                    g.DrawEllipse(pluma, cX, cY, sX, sY);
+                }
+
+                if (indice == 4)
+                {
+                    g.DrawRectangle(pluma, cX, cY, sX, sY);
+                }
+
+                if (indice == 5)
+                {
+                    g.DrawLine(pluma, cX, cY, sX, sY);
+                }
+            }
+        }
+
+        private void pbLienzo_MouseDown(object sender, MouseEventArgs e)
+        {
+            pintar = true;
+            py = e.Location;
+
+            cX = e.X;
+            cY = e.Y;
+        }
+
+        private void pbLienzo_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (pintar)
+            {
+                if (indice == 1)
+                {
+                    px = e.Location;
+                    g.DrawLine(pluma, px, py);
+                    py = px;
+                }
+
+                if (indice == 2)
+                {
+                    px = e.Location;
+                    g.DrawLine(borrador, px, py);
+                    py = px;
+                }
+            }
+            pbLienzo.Refresh();
+
+            x = e.X;
+            y = e.Y;
+            sX = e.X - cX;
+            sY = e.Y - cY;
+        }
+
+        private void pbLienzo_MouseUp(object sender, MouseEventArgs e)
+        {
+            pintar = false;
+
+            sX = x - cX;
+            sY = y - cY;
+
+            if (indice == 3)
+            {
+                g.DrawEllipse(pluma, cX, cY, sX, sY);
+            }
+
+            if (indice == 4)
+            {
+                g.DrawRectangle(pluma, cX, cY, sX, sY);
+            }
+
+            if (indice == 5)
+            {
+                g.DrawLine(pluma, cX, cY, sX, sY);
+            }
+        }
+
+        private void btnLapiz_Click(object sender, EventArgs e)
+        {
+            indice = 1;
+        }
+
+        private void btnBorrador_Click(object sender, EventArgs e)
+        {
+            indice = 2;
+        }
+
+        private void btnCirculo_Click(object sender, EventArgs e)
+        {
+            indice = 3;
+        }
+
+        private void btnCuadrado_Click(object sender, EventArgs e)
+        {
+            indice = 4;
+        }
+
+        private void btnLinea_Click(object sender, EventArgs e)
+        {
+            indice = 5;
+        }
+    }
+}
