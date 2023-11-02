9fef988bf718a6ce2cbc1ca4e240a9d118f9de53
Ulisesdelvillar0207 <ur226278@gmail.com>
2023-11-01 19:54:43 -0600

    quite un boton

--- Proyecto_Paint_Poo1/Proyecto_Paint_Poo1/Form1.Designer.cs
+++ Proyecto_Paint_Poo1/Proyecto_Paint_Poo1/Form1.Designer.cs
@@ -30,50 +30,36 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(363, 14);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(242, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(453, 96);
+            this.label1.Size = new System.Drawing.Size(304, 66);
             this.label1.TabIndex = 0;
             this.label1.Text = "Proyecto de no se que parcial \r\n\r\nProgramacion Orientada a Objetos \r\n";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(440, 239);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Location = new System.Drawing.Point(293, 155);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(284, 146);
+            this.button1.Size = new System.Drawing.Size(189, 95);
             this.button1.TabIndex = 1;
             this.button1.Text = "Paint";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(855, 216);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(260, 235);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1200, 692);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
@@ -86,7 +72,6 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
     }
 }
 
